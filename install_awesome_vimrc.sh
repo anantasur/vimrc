7cfdaf78d8a19de8ada449047b103a4432606265
Ananthu R V <ananthurv666@hotmail.com>
2016-04-05 08:26:33 +0530

    added vim as core editor for git when installing

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -6,7 +6,7 @@ source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
 source ~/.vim_runtime/vimrcs/extended.vim
-
+git config --global core.editor $(which vim)
 try
 source ~/.vim_runtime/my_configs.vim
 catch
--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -1,3 +1,4 @@
 cd ~/.vim_runtime
 cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
+git config --global core.editor $(which vim)
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
